edd36b559e4fd7dfe335d95c3c6f9f217923d2f7
Vinita Mathreja <100028773+vintaaa@users.noreply.github.com>
2025-06-06 21:47:55 +0530

    Add .next/static/chunks/src_app_page_tsx_eb1a539c._.js 1749226639279

---
+++ .next/static/chunks/src_app_page_tsx_eb1a539c._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_77b85d72._.js"
+],
+    source: "dynamic"
+});

aeb70f57e8ffa4c22de34f120c75b9db48deafb3
Vinita Mathreja <100028773+vintaaa@users.noreply.github.com>
2025-06-06 21:48:24 +0530

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1749226667855

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_13b5752c._.js",
+  "static/chunks/src_5f867d65._.js"
+],
+    source: "dynamic"
+});
